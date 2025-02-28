b01b87d5c5ef1e44d05905cabf0431c9dae38c87
Biswa Ranjan <biswaranjannayak2016@gmail.com>
2025-02-28 07:31:24 +0530

    NEW: datatype conversion

---
+++ basic/conversion.js
@@ -0,0 +1,26 @@
+// let score = "53";
+// let score = "53abc";
+// let score = null;
+let score = undefined;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof score);
+console.log(valueInNumber);
+
+// "53" => 53
+// "53abc" => NaN
+// null => o
+// true => 1; False =>0
+//  undefine => NaNS
+
+// let booleanValue = 1;
+// let booleanValue = 0;
+// let booleanValue = "";
+let booleanValue = "kunu";
+
+let convert = Boolean(booleanValue);
+console.log(convert);
+
+
