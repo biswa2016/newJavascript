1cc45402b43a27ae5bdd47e141b6246f67e06ede
Biswa Ranjan <biswaranjannayak2016@gmail.com>
2025-03-02 17:31:18 +0530

    New : Date in js

---
+++ basic/date.js
@@ -0,0 +1,25 @@
+let myDate = new Date();
+
+// console.log(myDate); //2025-03-02T11:46:25.768Z
+
+// console.log(myDate.toString()); //Sun Mar 02 2025 17:16:25 GMT+0530 (India Standard Time)
+
+
+// console.log(myDate.toDateString()); //Sun Mar 02 2025
+// console.log(myDate.toLocaleString()); //3/2/2025, 5:18:15 PM
+
+
+let myCreateDate = new Date(2024,12,23,5,3);
+// console.log(myCreateDate.toLocaleDateString()); //1/23/2025
+// console.log(myCreateDate.toLocaleTimeString()); //5:03:00 AM
+
+let forDate = Date.now();
+
+// console.log(forDate); //1740916571445
+// console.log(myCreateDate.getTime()); //1737588780000
+
+// console.log(Math.floor(Date.now()/1000));  //1740916659
+
+
+
+
